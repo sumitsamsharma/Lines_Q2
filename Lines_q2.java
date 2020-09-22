c6ec7a0d389f14e859c918aaf3621a75adb6cb46
sumitsamsharma <sharmasumit94286@gmail.com>
2020-09-22 22:04:32 +0530

    UC1

--- Lines_q2.java
+++ Lines_q2.java
@@ -4,6 +4,11 @@ public class Lines_q2
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Line Comparison Computation");
+		double x1=Math.floor(Math.random()*10);
+		double y1=Math.floor(Math.random()*10);
+		double x2=Math.floor(Math.random()*10);
+		double y2=Math.floor(Math.random()*10);
+		int length=(int)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+		System.out.println("Length is: "+length);
 	}
 }
