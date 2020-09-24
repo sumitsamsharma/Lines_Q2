3aeea0f33f5bc16fadbf7e75a1ed3fcbe62faa4e
sumitsamsharma <43275389+sumitsamsharma@users.noreply.github.com>
2020-09-24 17:06:47 +0530

    Update Lines_q2.java

--- Lines_q2.java
+++ Lines_q2.java
@@ -1,6 +1,6 @@
-package Lines;
+package Line_comparison;
 
-public class Lines_q2 
+public class Checking_lengths 
 {
 	public static void main(String[] args)
 	{
@@ -14,6 +14,9 @@ public class Lines_q2
 		double y4=Math.floor(Math.random()*10);
 		int length_1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		int length_2=(int)Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
+		
+		System.out.println("Line 1 length is: "+length_1);
+		System.out.println("Line 2 length is: "+length_2);
 		
 		if(length_1==length_2)
 		     System.out.println("Both lines are Equal");
