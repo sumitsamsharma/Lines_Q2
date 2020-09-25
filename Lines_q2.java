1ed40dd9e86a11c62062ff12f9a714245a6ee3dd
sumitsamsharma <43275389+sumitsamsharma@users.noreply.github.com>
2020-09-25 13:44:16 +0530

    Update Lines_q2.java

--- Lines_q2.java
+++ Lines_q2.java
@@ -12,10 +12,14 @@ public class Lines_q2
 		double y3=Math.floor(Math.random()*10);
 		double x4=Math.floor(Math.random()*10);
 		double y4=Math.floor(Math.random()*10);
-		int length_1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		int length_2=(int)Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
 		
-		if(length_1==length_2)
+		String length_1=String.valueOf((int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+		String length_2=String.valueOf((int)Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3)));
+		
+		System.out.println("Line 1 length is: "+length_1);
+		System.out.println("Line 2 length is: "+length_2);
+		
+		if(length_1.equals(length_2))
 		     System.out.println("Length is Equal");
 		else
 			System.out.println("Length is not Equal");
